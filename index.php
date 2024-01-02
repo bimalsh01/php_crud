c9ad2f73c57c536e7305ef59629e84fce7d007f7
Bimal Shrestha <69587963+bimalsh01@users.noreply.github.com>
2024-01-02 09:37:58 +0545

    final

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - Application</title>
+</head>
+<body>
+    
+    <?php
+        include_once "template/navbar.php";
+    ?>
+    <div>
+        <h1>Welcome To CRUD Application</h1>
+        <a href="template/register.php"><button>Register</button></a>
+        <a href="template/view.php"><button>View Users</button></a>
+    </div>
+
+</body>
+</html>
